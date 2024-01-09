9b6ae400781185c2c4fdbc3f0412669111633a3b
Pol Pastells <polpastells@gmail.com>
2024-01-09 16:58:29 +0100

    pandas read, head and tail

--- cli/app.py
+++ cli/app.py
@@ -1,12 +1,47 @@
+import os
+from ast import literal_eval
 from functools import update_wrapper
-import click
+from typing import Dict
 
+import click
+import pandas as pd
+
+# pd.options.display.max_columns = None
+
+CONFIG = {
+    "read_functions": {
+        ".csv": pd.read_csv,
+        ".tsv": pd.read_csv,
+        ".xlsx": pd.read_excel,
+    },
+    "args_read_functions": {".csv": {}, ".tsv": {"sep": "\t"}, ".xlsx": {}},
+    "to_functions": {
+        ".csv": lambda df, **kwargs: df.to_csv(kwargs.pop("buf"), **kwargs),
+        ".xlsx": lambda df, **kwargs: df.to_excel(kwargs.pop("buf"), **kwargs),
+    },
+}
+
+
+def args_to_dict(arg: str) -> Dict:
+    """Allow to parse string and try to interpret it from cli option format
+    "name_param=value|name_param=value" and return a dict"""
+    params_asdict = {}
+    if arg:
+        params = arg.split("|")
+        for p in params:
+            splitted = p.split("=")
+            try:
+                params_asdict[splitted[0]] = literal_eval(
+                    splitted[1]
+                )  # try to interpret the string for instance "True" -> True
+            except Exception:
+                params_asdict[splitted[0]] = splitted[1]  # fallback to string
+    return params_asdict
 
 
 @click.group(chain=True)
 def cli():
-    """CLI
-    """
+    """CLI"""
 
 
 # ---------------------------------------------------------------------------
@@ -15,7 +50,7 @@ def cli():
 # credits: https://github.com/pallets
 
 
-@cli.resultcallback()
+@cli.result_callback()
 def process_commands(processors):
     """This result callback is invoked with an iterable of all the chained
     subcommands.  As in this example each subcommand returns a function
@@ -63,3 +98,88 @@ def generator(f):
         yield from f(*args, **kwargs)
 
     return update_wrapper(new_func, f)
+
+
+@cli.command("read")
+@click.argument("filename", type=click.Path(exists=True), required=True)
+@click.option(
+    "-p",
+    "--params",
+    type=str,
+    default="",
+    help="""string representing params forwarded to pd.read function in format:
+    'name_param=value|name_param=value'""",
+)
+@generator
+def read_cmd(filename, params):
+    """read one file and convert it to a pandas data frame.
+    \b
+
+    Pandas read function are called under the hood thanks to a mapping with file extension.
+    """
+
+    if os.path.isfile(filename):
+        _, fextension = os.path.splitext(filename)
+        read_func = CONFIG["read_functions"]
+        pd_params = args_to_dict(params) if params else {}
+        pd_params = {**pd_params, **CONFIG["args_read_functions"][fextension]}
+        df = read_func[fextension](filename, **pd_params)
+        yield df
+
+
+@cli.command("head")
+@click.argument("lines", default=10, type=int, required=False)
+@processor
+def head_cmd(dfs, lines):
+    """Print to stdout the specified num of @lines starting from the top of the dataframe."""
+    for df in dfs:
+        click.echo(df.head(lines))
+        yield
+
+
+@cli.command("tail")
+@click.argument("lines", default=10, type=int, required=False)
+@processor
+def tail_cmd(dfs, lines):
+    """Print to stdout the specified num of @lines starting from the top of the dataframe."""
+    for df in dfs:
+        click.echo(df.tail(lines))
+        yield
+
+
+@cli.command("filter")
+@click.argument("expression", required=True, type=str)
+@processor
+def filter_cmd(dfs, expression):
+    """Execute a pd.query with @expression to the dataframe.
+    \b
+
+    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.query.html
+    """
+    for df in dfs:
+        df = df.query(expression)
+        yield df
+
+
+@cli.command("to")
+@click.option("-f", "--filename", type=click.Path())
+@click.option(
+    "-p",
+    "--params",
+    type=str,
+    default="",
+    help="string representing the params to be forwarded to the pd.to function in format: 'name_param=value|name_param=value'",
+)
+@processor
+def to_cmd(dfs, filename, params):
+    """Export dataframe, according to @filename extension and forward @params to the pd.to function associated.
+    \b
+
+    Currently supported extensions : .xlsx: to_excel | .csv: to_csv | .html: to_html
+    """
+    for df in dfs:
+        _, fextension = os.path.splitext(filename)
+        to_func = CONFIG["to_functions"][fextension]
+        pd_params = args_to_dict(params) if params else {}
+        to_func(df, buf=filename, **pd_params)
+        yield
